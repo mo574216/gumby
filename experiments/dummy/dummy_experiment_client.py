5188d55352b0e8fe262a82b634634c008e8b2afb
Seyedakbar Mostafavi <akbar.mostafavi@gmail.com>
2014-05-13 18:25:39 +0200

    Update dummy_experiment_client.py
    Adding Scenario File

--- experiments/dummy/dummy_experiment_client.py
+++ experiments/dummy/dummy_experiment_client.py
@@ -4,9 +4,9 @@
 # Filename: experiment_client.py
 # Description:
 # Author: Elric Milon
-# Maintainer:
+# Contributor: Seyedakbar Mostafavi
 # Created: Mon Sep  9 11:20:41 2013 (+0200)
-
+# Updated: Tue May 13 2014
 # Commentary:
 #
 #
@@ -44,9 +44,12 @@ from gumby.sync import ExperimentClient, ExperimentClientFactory
 from gumby.log import setupLogging
 from twisted.internet import reactor
 from twisted.python.log import msg, err, startLogging
+from gumby.scenario import ScenarioRunner
 
 class DummyExperimentClient(ExperimentClient):
     def startExperiment(self):
+        scenario_file_path = './'
+        self.scenario_runner = ScenarioRunner(scenario_file_path)
         msg("Starting dummy experiment (exiting in a couple of seconds)")
         msg("in-experiment DEFAULT_LEVEL")
         err("in-experiment ERROR_LEVEL")
